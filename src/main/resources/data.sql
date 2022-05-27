0669f36edaa61ee0935381585c4b19180cf43602
Abhishek Sharma <sharma.abhishek208@gmail.com>
2022-05-27 22:05:33 +0530

    Added sql data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,6 @@ values(10002, 'in28minutes', 'Learn SQL', now(), false);
 
 insert into todo(id, username, description, target_date, is_done)
 values(10003, 'in28minutes', 'Learn Java', now(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10004, 'abhi', 'Learn Spring', now(), false);
